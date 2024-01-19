5df302063d3aa3121fafec317983f90ea2167d7a
Tristan <tristan.ginet@etu.univ-smb.fr>
2024-01-19 10:30:40 +0100

    Initial Commit

---
+++ Looter/LooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooterApp.swift
+//  Looter
+//
+//  Created by Tristan GINET on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
